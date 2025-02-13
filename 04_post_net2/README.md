31a3bcb2a1a43f4d1c1dd30c3f2e1a9733fdbd5e
RobinWillson <robinwillson101@gmail.com>
2025-02-13 19:20:37 +0800

    0213aa

---
+++ 04_post_net2/README.md
@@ -0,0 +1,9 @@
+### Reference
+* https://www.youtube.com/watch?v=oGQBp0xbRFw
+
+### Goal
+* 基於 NextJS@15 & MUI@6
+* 試作以上 Post Net 的網站
+* 測試 Mui for NextJS
+  https://mui.com/material-ui/integrations/nextjs/
+
---
+++ 04_post_net2/app/page.jsx
@@ -0,0 +1,14 @@
+import { Box, AppBar, Typography } from "@mui/material";
+const Home = () => {
+  return (
+    <Box>
+      {/* Nav Bar */ }
+      <AppBar>
+        <Typography variant="h6">POST.NET</Typography>
+      </AppBar>
+      {/* Main Container */ }
+    </Box>
+  );
+};
+
+export default Home;
